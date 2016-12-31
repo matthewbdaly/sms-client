15511bda06ace1605ea63f688b20d0d6e7248a11
Matthew Daly <matthewbdaly@gmail.com>
2016-12-31 22:54:20 +0000

    Amended driver interface

--- src/Contracts/Driver.php
+++ src/Contracts/Driver.php
@@ -4,4 +4,9 @@ namespace Matthewbdaly\SMS\Contracts;
 
 interface Driver
 {
+    public function getDriver();
+
+    public function getEndpoint();
+
+    public function sendRequest(array $message);
 }
