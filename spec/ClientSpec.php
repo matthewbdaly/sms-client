3554cb39e078f176213a11cf3ba390a2c2fcc0e4
Matthew Daly <matthewbdaly@gmail.com>
2016-12-31 23:09:23 +0000

    Added constructor

--- spec/ClientSpec.php
+++ spec/ClientSpec.php
@@ -5,9 +5,15 @@ namespace spec\Matthewbdaly\SMS;
 use Matthewbdaly\SMS\Client;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use Matthewbdaly\SMS\Contracts\Driver;
 
 class ClientSpec extends ObjectBehavior
 {
+    function let(Driver $driver)
+    {
+        $this->beConstructedWith($driver);
+    }
+
     function it_is_initializable()
     {
         $this->shouldHaveType(Client::class);
--- src/Client.php
+++ src/Client.php
@@ -2,6 +2,12 @@
 
 namespace Matthewbdaly\SMS;
 
+use Matthewbdaly\SMS\Contracts\Driver;
+
 class Client
 {
+    public function __construct(Driver $driver)
+    {
+        // TODO: write logic here
+    }
 }
