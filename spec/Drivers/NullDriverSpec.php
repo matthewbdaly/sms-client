556fda5bd7f88c098b892341c57113fbd0a9cf39
Matthew Daly <matthewbdaly@gmail.com>
2017-04-17 18:40:09 +0100

    Set up constructor

--- spec/Drivers/NullDriverSpec.php
+++ spec/Drivers/NullDriverSpec.php
@@ -5,9 +5,16 @@ namespace spec\Matthewbdaly\SMS\Drivers;
 use Matthewbdaly\SMS\Drivers\NullDriver;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\Psr7\Response as GuzzleResponse;
 
 class NullDriverSpec extends ObjectBehavior
 {
+    function let(GuzzleClient $client, GuzzleResponse $response)
+    {
+        $this->beConstructedWith($client, $response);
+    }
+
     function it_is_initializable()
     {
         $this->shouldHaveType(NullDriver::class);
--- src/Drivers/NullDriver.php
+++ src/Drivers/NullDriver.php
@@ -3,9 +3,21 @@
 namespace Matthewbdaly\SMS\Drivers;
 
 use Matthewbdaly\SMS\Contracts\Driver;
+use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\Psr7\Response as GuzzleResponse;
 
 class NullDriver implements Driver
 {
+    protected $client;
+
+    protected $response;
+
+    public function __construct(GuzzleClient $client, GuzzleResponse $response)
+    {
+        $this->client = $client;
+        $this->response = $response;
+    }
+
     public function getDriver()
     {
     }
