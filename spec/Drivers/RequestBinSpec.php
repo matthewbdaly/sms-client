3520750220680f846dca2d188fc18c8761dd41c1
Matthew Daly <matthewbdaly@gmail.com>
2017-08-26 21:19:17 +0100

    Added base methods for RequestBin driver

--- spec/Drivers/RequestBinSpec.php
+++ spec/Drivers/RequestBinSpec.php
@@ -5,11 +5,42 @@ namespace spec\Matthewbdaly\SMS\Drivers;
 use Matthewbdaly\SMS\Drivers\RequestBin;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\Psr7\Response as GuzzleResponse;
 
 class RequestBinSpec extends ObjectBehavior
 {
+    function let(GuzzleClient $client, GuzzleResponse $response)
+    {
+        $this->beConstructedWith($client, $response);
+    }
+
     function it_is_initializable()
     {
         $this->shouldHaveType(RequestBin::class);
+    }
+
+    function it_implements_interface()
+    {
+        $this->shouldImplement('Matthewbdaly\SMS\Contracts\Driver');
+    }
+
+    function it_returns_the_driver_name()
+    {
+        $this->getDriver()->shouldReturn('RequestBin');
+    }
+
+    function it_returns_the_driver_endpoint()
+    {
+        $this->getEndpoint()->shouldReturn(null);
+    }
+
+    function it_sends_the_request()
+    {
+        $msg = [
+            'to'      => '+44 01234 567890',
+            'content' => 'Just testing',
+        ];
+        $this->sendRequest($msg)->shouldReturn(true);
     }
 }
--- src/Drivers/RequestBin.php
+++ src/Drivers/RequestBin.php
@@ -2,6 +2,34 @@
 
 namespace Matthewbdaly\SMS\Drivers;
 
-class RequestBin
+use Matthewbdaly\SMS\Contracts\Driver;
+use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\Psr7\Response as GuzzleResponse;
+
+class RequestBin implements Driver
 {
+    protected $client;
+
+    protected $response;
+
+    public function __construct(GuzzleClient $client, GuzzleResponse $response)
+    {
+        $this->client = $client;
+        $this->response = $response;
+    }
+
+    public function getDriver()
+    {
+        // TODO: write logic here
+    }
+
+    public function getEndpoint()
+    {
+        // TODO: write logic here
+    }
+
+    public function sendRequest($argument1)
+    {
+        // TODO: write logic here
+    }
 }
