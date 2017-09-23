77a12854296c3416be1a77da583205132681c9be
Matthew Daly <matthewbdaly@gmail.com>
2017-09-23 14:15:14 +0100

    Commented last driver

--- src/Drivers/RequestBin.php
+++ src/Drivers/RequestBin.php
@@ -10,16 +10,48 @@ use GuzzleHttp\Exception\ServerException;
 use GuzzleHttp\Psr7\Response as GuzzleResponse;
 use Matthewbdaly\SMS\Contracts\Driver;
 
+/**
+ * Driver for RequestBin
+ */
 class RequestBin implements Driver
 {
+
+    /**
+     * Guzzle client
+     *
+     * @var $client
+     */
     protected $client;
 
+    /**
+     * Guzzle response
+     *
+     * @var $response
+     */
     protected $response;
 
+    /**
+     * Path
+     *
+     * @var $path
+     */
     private $path;
 
+    /**
+     * Endpoint
+     *
+     * @var $endpoint
+     */
     private $endpoint = 'https://requestb.in/';
 
+    /**
+     * Constructor
+     *
+     * @param GuzzleClient   $client   The Guzzle Client instance.
+     * @param GuzzleResponse $response The Guzzle response instance.
+     * @param array          $config   The configuration array.
+     * @return void
+     */
     public function __construct(GuzzleClient $client, GuzzleResponse $response, array $config)
     {
         $this->client = $client;
@@ -27,16 +59,36 @@ class RequestBin implements Driver
         $this->path = $config['path'];
     }
 
+    /**
+     * Get driver name
+     *
+     * @return string
+     */
     public function getDriver(): string
     {
         return 'RequestBin';
     }
 
+    /**
+     * Get endpoint URL
+     *
+     * @return string
+     */
     public function getEndpoint(): string
     {
         return $this->endpoint.$this->path;
     }
 
+    /**
+     * Send the request
+     *
+     * @param array $message An array containing the message.
+     * @return boolean
+     * @throws \Matthewbdaly\SMS\Exceptions\ClientException Client exception.
+     * @throws \Matthewbdaly\SMS\Exceptions\ServerException Server exception.
+     * @throws \Matthewbdaly\SMS\Exceptions\NetworkException Network exception.
+     * @throws \Matthewbdaly\SMS\Exceptions\ConnectException Connect exception.
+     */
     public function sendRequest(array $message): bool
     {
         try {
