8401d9aaa97c8e8cefaa6136a21d56f83fa54bbb
Matthew Daly <matthewbdaly@gmail.com>
2017-09-23 14:17:47 +0100

    Commented the client

--- src/Client.php
+++ src/Client.php
@@ -4,21 +4,46 @@ namespace Matthewbdaly\SMS;
 
 use Matthewbdaly\SMS\Contracts\Driver;
 
+/**
+ * SMS client
+ */
 class Client
 {
+    /**
+     * Driver to use
+     *
+     * @var $driver
+     */
     private $driver;
 
+    /**
+     * Constructor
+     *
+     * @param Driver $driver The driver to use.
+     * @return void
+     */
     public function __construct(Driver $driver)
     {
         $this->driver = $driver;
     }
 
+    /**
+     * Get the driver name
+     *
+     * @return string
+     */
     public function getDriver(): string
     {
         return $this->driver->getDriver();
     }
 
-    public function send($msg): bool
+    /**
+     * Send the message
+     *
+     * @param array $msg The message array.
+     * @return boolean
+     */
+    public function send(array $msg): bool
     {
         return $this->driver->sendRequest($msg);
     }
