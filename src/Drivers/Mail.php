4cb2696d254fff20e922baf465f2ccf37c287ad5
Matthew Daly <matthewbdaly@gmail.com>
2017-11-04 23:32:39 +0000

    Added interface to mail driver

--- src/Drivers/Mail.php
+++ src/Drivers/Mail.php
@@ -3,20 +3,21 @@
 namespace Matthewbdaly\SMS\Drivers;
 
 use Matthewbdaly\SMS\Contracts\Mailer;
+use Matthewbdaly\SMS\Contracts\Driver;
 
-class Mail
+class Mail implements Driver
 {
     public function __construct($argument1)
     {
         // TODO: write logic here
     }
 
-    public function getDriver()
+    public function getDriver(): string
     {
         return 'Mail';
     }
 
-    public function getEndpoint()
+    public function getEndpoint(): string
     {
         return '';
     }
