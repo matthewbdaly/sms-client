deda485eb8478d2bf1e55e86c95ef7baf035369f
Matthew Daly <matthewbdaly@gmail.com>
2017-11-05 17:13:45 +0000

    Added outline of TextLocal driver

---
+++ spec/Drivers/TextLocalSpec.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace spec\Matthewbdaly\SMS\Drivers;
+
+use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\ClientInterface as GuzzleInterface;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Response as GuzzleResponse;
+use Psr\Http\Message\ResponseInterface;
+use Matthewbdaly\SMS\Drivers\TextLocal;
+use PhpSpec\ObjectBehavior;
+
+class TextLocalSpec extends ObjectBehavior
+{
+    public function let(GuzzleInterface $client, ResponseInterface $response)
+    {
+        $config = [
+            'api_key' => 'blah',
+        ];
+        $this->beConstructedWith($client, $response, $config);
+    }
+
+    public function it_is_initializable()
+    {
+        $this->shouldHaveType(TextLocal::class);
+    }
+
+    public function it_implements_interface()
+    {
+        $this->shouldImplement('Matthewbdaly\SMS\Contracts\Driver');
+    }
+
+    public function it_returns_the_driver_name()
+    {
+        $this->getDriver()->shouldReturn('TextLocal');
+    }
+
+    public function it_returns_the_driver_endpoint()
+    {
+        $this->getEndpoint()->shouldReturn('https://api.txtlocal.com/send/');
+    }
+
+    public function it_sends_the_request(ResponseInterface $response)
+    {
+        $msg = [
+            'to'      => '+44 01234 567890',
+            'content' => 'Just testing',
+        ];
+        $mock = new MockHandler(
+            [
+            new GuzzleResponse(201),
+            ]
+        );
+        $handler = HandlerStack::create($mock);
+        $client = new GuzzleClient(['handler' => $handler]);
+        $config = [
+            'api_key' => 'MY_DUMMY_API_KEY',
+        ];
+        $this->beConstructedWith($client, $response, $config);
+        $this->sendRequest($msg)->shouldReturn(true);
+    }
+}
